8302089e0fb9634c44e1e8b7b198a4c3e2d1b482
Łukasz Lenart <lenartluk@gmail.com>
2026-01-01 19:44:45 +0100

    Initial Commit

---
+++ TripsBook/TripsBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  TripsBookApp.swift
+//  TripsBook
+//
+//  Created by Łukasz Lenart on 01/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct TripsBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
